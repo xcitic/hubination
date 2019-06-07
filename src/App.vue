343e1ae0f4e8ee126fd47e46e61278c8595d52b3
xcitic <sami@ecompx.com>
2019-06-07 19:45:42 +0200

    Complete tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="pb-2">
+      <icons :icon="['fas', 'star']" />
+       Hubination
+     </h1>
+
+    <DataTable :showRepos="showRepos" :repositories="repositories" />
+
+    <div v-if="loading" class="justify-content-center">
+      <icons :icon="['fas', 'spinner']" class="fa-spinner" />
+    </div>
+
+    <div class="my-4">
+      <ul class="pagination pagination-md justify-content-center text-center">
+        <li class="page-item" :class="page === 1 ? 'disabled' : ''">
+          <a
+             class="page-link"
+             @click="prevPage"
+          >
+            Previous
+         </a>
+        </li>
+        <li class="page-link" style="background-color: inherit">
+          {{ page }} of {{ lastPage }}
+        </li>
+        <li class="page-item" :class="page === lastPage ? 'disabled' : ''">
+          <a class="page-link"
+            @click="nextPage"
+          >
+            Next
+          </a>
+        </li>
+      </ul>
+    </div><!--./Pagination -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataTable from './components/DataTable.vue'
+import axios from 'axios'
+const githubAPI = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100&page='
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+ components: {
+  DataTable
+ },
+
+ data () {
+   return {
+     githubPage: 1,
+     repositories: [],
+     page: 1,
+     loading: false,
+     perPage: 20
+   }
+ },
+
+mounted () {
+  this.fetchData()
+ },
+
+ computed: {
+   showRepos () {
+     let start = (this.page - 1) * this.perPage
+     let end = start + this.perPage
+     this.loading = false
+     return this.repositories.slice(start, end)
+   },
+
+   lastPage () {
+     let length = this.repositories.length
+     return length / this.perPage
+  }
+
+ },
+
+ methods: {
+   fetchData () {
+     this.loading = true
+     axios.get(githubAPI + this.githubPage)
+       .then(({data}) => {
+         this.repositories = this.repositories.concat(data.items)
+        })
+       .catch((err) => {
+         throw err
+       })
+    },
+
+    prevPage() {
+      this.page--
+      window.scrollTo({top: 0, behavior: 'smooth'})
+    },
+
+    nextPage() {
+      if (this.page == this.lastPage - 1 ) {
+        this.githubPage++
+        this.fetchData()
+      }
+      this.page++
+      window.scrollTo({top: 0, behavior: 'smooth'})
+    }
+
   }
 }
 </script>
 
 <style>
+@keyframes spinner {
+  to { transform: rotate(360deg); }
+}
+.fa-spinner {
+ animation: spinner 1s linear infinite;
+}
+a:hover {
+  cursor: pointer;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner, faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSpinner, faStar)
+Vue.component('icons', FontAwesomeIcon)
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
